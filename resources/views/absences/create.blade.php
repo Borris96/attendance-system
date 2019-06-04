2ca062dfe4145c379421d6680d58b153f8015ad2
Borris96 <borrispoe96@gmail.com>
2019-06-04 05:48:07 +0000

    使表单保存失败时记住原来的选项

--- resources/views/absences/create.blade.php
+++ resources/views/absences/create.blade.php
@@ -14,7 +14,7 @@
         <select name="staff_id" id="name_select">
           <option value=""> -----请选择----- </option>
           @foreach($staffs as $staff)
-            <option value="{{ $staff->id }}"> {{ $staff->staffname }} </option>
+            <option value="{{ $staff->id }} " @if(old('staff_id') == $staff->id) selected @endif> {{ $staff->staffname }} </option>
           @endforeach
         </select>
 
@@ -25,15 +25,24 @@
          <td>
           <select name="absence_type">
             <option value=""> -----请选择----- </option>
-            <option value="事假">事假</option><option value='年假'>年假</option><option value='病假'>病假</option><option value='调休'>调休</option>
+            <option value="事假" @if(old('absence_type') == '事假') selected @endif>事假</option>
+            <option value='年假' @if(old('absence_type') == '年假') selected @endif>年假</option>
+            <option value='病假' @if(old('absence_type') == '病假') selected @endif>病假</option>
+            <option value='调休' @if(old('absence_type') == '调休') selected @endif>调休</option>
           </select>
         </td>
     </tr>
     <tr>
         <td class="tableleft">请假时间</td>
         <td>
-          <input type="datetime-local" name="absence_start_time" /> &nbsp;至&nbsp;
-          <input type="datetime-local" name="absence_end_time"/>
+          <input type="datetime-local" name="absence_start_time"
+          @if (old('absence_start_time')!=null) value="{{ date('Y-m-d',strtotime(old('absence_start_time'))).'T'.date('H:i',strtotime(old('absence_start_time'))) }}"
+          @endif
+          /> &nbsp;至&nbsp;
+          <input type="datetime-local" name="absence_end_time"
+          @if (old('absence_end_time')!=null) value="{{ date('Y-m-d',strtotime(old('absence_end_time'))).'T'.date('H:i',strtotime(old('absence_end_time'))) }}"
+          @endif
+          />
         </td>
     </tr>
     <tr>
@@ -41,14 +50,14 @@
         <td>
           <select name="approve">
             <option value=""> -----请选择----- </option>
-            <option value=1>是</option><option value=0>否</option>
+            <option value=1 @if(old('approve') == '是') selected @endif>是</option><option value=0 @if(old('approve') == '否') selected @endif>否</option>
           </select>
         </td>
     </tr>
     <tr>
         <td class="tableleft">备注</td>
         <td>
-          <textarea name="note" id="" rows="5" placeholder=""></textarea>
+          <textarea name="note" id="" rows="5" placeholder=""> {{ old('note') }} </textarea>
         </td>
     </tr>
     <tr>
