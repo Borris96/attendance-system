12a0ab58704dcbaa73d590c03495bc4871162d6c
Borris96 <borrispoe96@gmail.com>
2019-06-18 02:13:23 +0000

    push to heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,12 +18,12 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(DepartmentsTableSeeder::class);
         $this->call(PositionsTableSeeder::class);
-        $this->call(StaffsTableSeeder::class);
-        $this->call(StaffworkdaysTableSeeder::class);
-        $this->call(ExtraWorksTableSeeder::class);
-        $this->call(AbsencesTableSeeder::class);
-        $this->call(HolidaysTableSeeder::class);
-        $this->call(LieusTableSeeder::class);
+        // $this->call(StaffsTableSeeder::class);
+        // $this->call(StaffworkdaysTableSeeder::class);
+        // $this->call(ExtraWorksTableSeeder::class);
+        // $this->call(AbsencesTableSeeder::class);
+        // $this->call(HolidaysTableSeeder::class);
+        // $this->call(LieusTableSeeder::class);
         Model::reguard();
     }
 
