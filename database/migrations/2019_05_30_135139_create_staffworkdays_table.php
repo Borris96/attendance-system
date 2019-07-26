148f6120d56d07763e54becba8400909312b4dfb
Borris96 <borrispoe96@gmail.com>
2019-07-26 06:26:28 +0000

    准备更新工作时长

--- database/migrations/2019_05_30_135139_create_staffworkdays_table.php
+++ database/migrations/2019_05_30_135139_create_staffworkdays_table.php
@@ -14,7 +14,7 @@ class CreateStaffworkdaysTable extends Migration
     public function up()
     {
         Schema::create('staffworkdays', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->unsignedInteger('staff_id');
             $table->string('workday_name',10);
             $table->boolean('is_work');
