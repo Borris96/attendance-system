2a60ae0db0f1ee0eca397fe25c98a513ea137472
Borris96 <borrispoe96@gmail.com>
2019-07-27 01:54:47 +0000

    新建排班更新表

---
+++ database/migrations/2019_07_27_095158_create_staffworkday_updates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffworkdayUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffworkday_updates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('staffworkday_id');
+            $table->unsignedInteger('staff_id');
+            $table->string('workday_name',10);
+            $table->boolean('is_work');
+            $table->time('work_time')->nullable();
+            $table->time('home_time')->nullable();
+            $table->decimal('duration',5,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffworkday_updates');
+    }
+}
